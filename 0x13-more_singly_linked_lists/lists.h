0bb8e7f045211e6bb66526516e701ac9ab890c97
chaymae <ouklichchaymae@gmail.com>
2023-04-28 23:00:06 -0700

    listint len

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -15,4 +17,5 @@ typedef struct listint_s
 } listint_t;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 #endif
